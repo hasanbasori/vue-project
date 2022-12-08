0fb4e26837f53a1317eb339669f309c365b993cc
hasanbasori.sa <hasanbasori.sa@gmail.com>
2022-12-08 14:10:41 +0700

    Attribute Biding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="app">
+    <!-- การ biding สามารถที่ขะใช้คำว่า v-bind:src หรือ :src คือความหมายเดียวกัน -->
+    <img v-bind:src="picture" v-bind:width="size" :height="size" />
     <h2 align="left">Full name : {{ getFullName() }}</h2>
     <h2 align="left">age : {{ age }}</h2>
     <p>Address : <span v-html="address"></span></p>
+    <a v-bind:href="socialURL" target="_blank">Facebook</a>
   </div>
 </template>
 
@@ -16,11 +19,14 @@ export default {
       age: 25,
       weigh: 130.5,
       address: '<strong>Bangkok</strong>',
+      picture: 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png',
+      size: 150,
+      socialURL: 'https://www.facebook.com/hasanbasori.sa',
     };
   },
   methods: {
     getFullName() {
-      return `${this.firstName + " " + this.lastName}`
+      return `${this.firstName + ' ' + this.lastName}`;
     },
   },
 };
