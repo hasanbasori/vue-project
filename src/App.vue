bd850d71e90da77f31a88f5ddefff55d480f9e2a
hasanbasori.sa <hasanbasori.sa@gmail.com>
2022-12-08 14:17:06 +0700

    data array

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="app">
-    <!-- การ biding สามารถที่ขะใช้คำว่า v-bind:src หรือ :src คือความหมายเดียวกัน -->
     <img v-bind:src="picture" v-bind:width="size" :height="size" />
     <h2 align="left">Full name : {{ getFullName() }}</h2>
     <h2 align="left">age : {{ age }}</h2>
     <p>Address : <span v-html="address"></span></p>
-    <a v-bind:href="socialURL" target="_blank">Facebook</a>
+    <p>
+      Facebook Link : <a v-bind:href="socialURL" target="_blank">Facebook</a>
+    </p>
+    <p>Hobby : </p>
+    <ul>
+      <li>{{ hobby[0] }}</li>
+      <li>{{ hobby[1] }}</li>
+      <li>{{ hobby[2] }}</li>
+    </ul>
   </div>
 </template>
 
@@ -22,6 +29,7 @@ export default {
       picture: 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png',
       size: 150,
       socialURL: 'https://www.facebook.com/hasanbasori.sa',
+      hobby: ['read a book', 'play a game', 'listen music'],
     };
   },
   methods: {
